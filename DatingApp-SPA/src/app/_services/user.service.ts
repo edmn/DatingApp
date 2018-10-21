94ef30ae612589f99f4e2e715388f9521588c75c
EdDev <eddymartin@gmail.com>
2018-10-21 22:41:09 +0100

    End of Section 9

---
+++ DatingApp-SPA/src/app/_services/user.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../_models/user';
+
+// const httpOptions = {
+//   headers: new HttpHeaders({ // need a sapce after bearer
+//     'Authorization': 'Bearer ' + localStorage.getItem('token')
+//   })
+// };
+// pass in object into the get request. but now using an interceptor
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  baseUrl = environment.apiUrl;
+
+constructor(private http: HttpClient) {}
+
+getUsers(): Observable<User[]> {
+  return this.http.get<User[]>(this.baseUrl + 'users');
+}
+
+getUser(id): Observable<User> {
+  return this.http.get<User>(this.baseUrl + 'users/' + id);
+}
+
+}
